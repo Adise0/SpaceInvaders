db15a6afbf64b9bd5cd6a51d480cd16552de2346
Adise <admin@pauibannez.me>
2026-02-26 21:58:58 +0100

    added enemies movemennt

--- Assets/Scripts/EnemyController/EnemyController.cs
+++ Assets/Scripts/EnemyController/EnemyController.cs
@@ -27,19 +27,49 @@ namespace SpaceInvaders
     private const short Rows = 5;
     /// <summary>The number of enemies per row</summary>
     private const short EnemiesPerRow = 11;
-    private const float InitialRowY = 4f;
-    private const float RowHeight = 0.8f;
+
+    [Header("Gameplay")]
+    /// <summary>The movement timer</summary>
+    private float moveTimer = 0;
+    /// <summary>The delay between movements</summary>
+    [SerializeField] private float movementDelay;
+    /// <summary>The current move direciton</summary>
+    [SerializeField] private Vector2 currentDirection = Vector2.right;
+    /// <summary>The pixels per move</summary>
+    [SerializeField] const int MoveStepPx = 8;
 
 
-    [SerializeField] private Transform mapHolder;
+
+    [Header("Pixel perfect settings")]
+    /// <summary>the pixels per unit. This matches the sprite ppu</summary>
+    [SerializeField] private const int PixelsPerUnit = 32;
+    private const float UnitsPerPixel = 1f / PixelsPerUnit;
+
+    [Header("Formation settings")]
+    /// <summary>The Y start position in pixels</summary>
+    [SerializeField] private const short StartYpx = 120;
+    /// <summary>The row spacing in pixels</summary>
+    [SerializeField] private const short RowStepYpx = 16;
+    /// <summary>The column spacing in pixels</summary>
+    [SerializeField] private const short ColStepXpx = 16;
+
+    [SerializeField] private const short MinXPx = -98;
+    [SerializeField] private const short MaxXPx = 98;
+
+    private Vector2 step = new Vector2(MoveStepPx * UnitsPerPixel, 0);
+
+    [Header("Assets")]
+    /// <summary>The enemy holder transform</summary>
+    [SerializeField] private Transform enemyHolder;
     /// <summary>The base enemy prefab</summary>
     [SerializeField] private GameObject enemyPrefab;
     /// <summary>The store of enemy sprites</summary>
     [SerializeField] private Sprite[] enemySprites = new Sprite[Rows];
-    /// <summary>The store of all enemies</summary>
-    private List<GameObject> enemies;
+
     /// <summary>The row transforms</summary>
     private Transform[] rows = new Transform[Rows];
+    /// <summary>The store of all enemies</summary>
+    private List<GameObject> enemies = new();
     #endregion
 
     #region Unity
@@ -58,6 +88,14 @@ namespace SpaceInvaders
       InitializeEnemies();
       #endregion
     }
+
+    /// <summary>Ran by Unity each frame</summary>
+    private void Update()
+    {
+      #region Update
+      MoveEnemies();
+      #endregion
+    }
     #endregion
 
     #region Methods
@@ -65,23 +103,64 @@ namespace SpaceInvaders
     private void InitializeEnemies()
     {
       #region InitializeEnemies
+      int formationWidthPx = (EnemiesPerRow - 1) * ColStepXpx;
+      int halfWidthPx = formationWidthPx / 2;
+
       for (short row = 0; row < Rows; row++)
       {
+        int rowYpx = StartYpx - row * RowStepYpx;
+
         GameObject rowGO = new GameObject($"Row-{row}");
-        rowGO.transform.SetParent(mapHolder);
-        rowGO.transform.position = new Vector2(0, InitialRowY - RowHeight * row);
+        rowGO.transform.SetParent(enemyHolder, worldPositionStays: false);
+        rowGO.transform.localPosition = new Vector2(0f, rowYpx * UnitsPerPixel);
+
         rows[row] = rowGO.transform;
 
         for (short enemyIndex = 0; enemyIndex < EnemiesPerRow; enemyIndex++)
         {
           GameObject enemy = Instantiate(enemyPrefab, rows[row]);
-          enemy.transform.position = new Vector2(enemyIndex - (EnemiesPerRow * 0.5f), rows[row].position.y);
+          int xPx = enemyIndex * ColStepXpx - halfWidthPx;
+          enemy.transform.localPosition = new Vector3(xPx * UnitsPerPixel, 0f, 0f);
           enemy.GetComponent<SpriteRenderer>().sprite = enemySprites[row];
+          enemies.Add(enemy);
         }
       }
       #endregion
     }
-    #endregion
 
+    /// <summary>Moves the enemies</summary>
+    private void MoveEnemies()
+    {
+      #region MoveEnemies
+      moveTimer += Time.deltaTime;
+      if (moveTimer < movementDelay) return;
+      moveTimer = 0;
+
+
+
+      enemyHolder.position += (Vector3)(currentDirection * step);
+      if (!HasReachedWall()) return;
+
+      currentDirection *= -1;
+      enemyHolder.position += (Vector3)(Vector2.down * step);
+      #endregion
+    }
+
+    /// <summary>Checks if any spacechip has hit a wall</summary>
+    private bool HasReachedWall()
+    {
+      #region HasReachedWall
+      foreach (GameObject enemy in enemies)
+      {
+        if (!enemy) continue;
+
+        if ((enemy.transform.position.x <= MinXPx * UnitsPerPixel && currentDirection == Vector2.left)
+        || (enemy.transform.position.x >= MaxXPx * UnitsPerPixel && currentDirection == Vector2.right)
+        ) return true;
+      }
+      return false;
+      #endregion
+    }
+    #endregion
   }
 }
--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -1,5 +1,6 @@
 
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace SpaceInvaders
 {
@@ -7,10 +8,60 @@ namespace SpaceInvaders
   public class PlayerController : MonoBehaviour
   {
     #region Data
+    /// <summary>The player movement speed</summary>
     [SerializeField] private float movementSpeed;
+    /// <summary>The atack coolddown</summary>
     [SerializeField] private float atackCooldown;
-    [SerializeField] private float yLevel;
-    [SerializeField] Vector2 xBounds;
+    /// <summary>The x bounds of the movement</summary>
+    [SerializeField] private (float, float) xBounds = (-6f, 6f);
+
+    /// <summary>Input action in charge of movement</summary>
+    private InputAction moveAction;
     #endregion
+
+
+    #region Unity
+    /// <summary>Ran by Unity on load</summary>
+    private void Awake()
+    {
+      #region Awake
+      BindControls();
+      #endregion
+    }
+
+    /// <summary>Ran by Unity each frame</summary>
+    private void Update()
+    {
+      #region Update
+      MovePlayer();
+      #endregion
+    }
+    #endregion
+
+    #region Methods
+    /// <summary>Binds the controls from the input system</summary>
+    private void BindControls()
+    {
+      #region BindControls
+      moveAction = InputSystem.actions["Move"];
+      #endregion
+    }
+
+    /// <summary>Handles the player movement</summary>
+    private void MovePlayer()
+    {
+      #region MovePlayer
+      Vector2 dir = moveAction.ReadValue<Vector2>();
+      if (dir == Vector2.zero) return;
+
+      var (min, max) = xBounds;
+
+      if (transform.position.x > min && transform.position.x < max) transform.position += (Vector3)dir * movementSpeed * Time.deltaTime;
+      if (transform.position.x < min) transform.position = new Vector2(min, transform.position.y);
+      if (transform.position.x > max) transform.position = new Vector2(max, transform.position.y);
+      #endregion
+    }
+    #endregion
+
   }
 }
