202e1756b763ecf98d8492c19cff0c63672571fa
Adise <admin@pauibanez.me>
2026-03-01 12:57:39 +0100

    added bullet init and collision detection

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using SpaceInvaders.Data;
 using UnityEngine;
 
 namespace SpaceInvaders
@@ -6,6 +8,54 @@ namespace SpaceInvaders
   {
     #region Data
     public BulletType type;
+    private int stepPx;
+    private Vector2 dir;
+    private Vector2 prevPos;
     #endregion
+
+
+    #region Unity
+    /// <summary>Ran by Unity each fixed tick</summary>
+    private void FixedUpdate()
+    {
+      #region FixedUpdate
+      Move();
+      #endregion
+    }
+
+    /// <summary>Ran by unity on trigger enter</summary>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      #region OnTriggerEnter2D
+      IDamagable foundComponent;
+      TryGetComponent<IDamagable>(out foundComponent);
+      foundComponent?.TakDamage(this);
+      #endregion
+    }
+    #endregion
+
+
+    #region Methods
+    /// <summary>Initializes this bullet</summary>
+    private void Init(BulletType type)
+    {
+      #region Init
+      this.type = type;
+      stepPx = type == BulletType.Player ? 4 : 3;
+      dir = type == BulletType.Player ? Vector2.up : Vector2.down;
+      #endregion
+    }
+
+
+    /// <summary>Moves the bullet each tick</summary>
+    private void Move()
+    {
+      #region Move
+      prevPos = transform.position;
+      transform.position += (Vector3)(dir * stepPx * PixelPerfect.UnitsPerPixel);
+      #endregion
+    }
+    #endregion
+
   }
 }
